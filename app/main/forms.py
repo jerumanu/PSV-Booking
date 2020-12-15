4c12799b561243a91a4ae1125d0b1c337513c5b2
sharon0812 <anyangosharon@gmail.com>
2020-12-15 21:08:35 +0300

    modify form.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,31 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField, RadioField,SelectField
+from wtforms import StringField, TextAreaField, SubmitField,RadioField,SelectField
 from wtforms.validators import Required
 
-class CommentsForm(FlaskForm):
-    comment = TextAreaField('Comment', validators=[Required()])
-
-    
-
-    submit = SubmitField('SUBMIT')
-
-class PitchForm(FlaskForm):
-    category_id = SelectField('Select Category', choices=[('1', 'Interview'), ('2', 'Pick Up Lines'), ('3', 'Promotion')])
-    content = TextAreaField('make a pitch', validators=[Required()])
-    submit = SubmitField('Create Pitch')
-
-class UpvoteForm(FlaskForm):
-    '''
-    Class to create a wtf form for upvoting a pitch
-    '''
-    submit = SubmitField('Upvote')
-
-class DownvoteForm(FlaskForm):
-    '''
-    Class to create a wtf form for downvoting a pitch
-    '''  
-    submit = SubmitField('Downvote')
-
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('What about you.',validators = [Required()])
+    bio = TextAreaField('Whats about yourself.',validators = [Required()])
     submit = SubmitField('Submit')
+    
+class PostForm(FlaskForm):
+    post_title = StringField('Title')
+    description = TextAreaField('Description', validators=[Required()])
+    submit = SubmitField('Submit')
+    
+class CommentForm(FlaskForm):
+    comment = TextAreaField('write a comment',validators=[Required()])
+    submit = SubmitField('comment')
