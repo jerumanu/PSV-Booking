fbcd0f5495c5f3d6fd244e2005d2844df599f6cc
jerumanu <emmanueljeru200@gmail.com>
2020-12-18 10:07:54 +0300

    add changes

--- app/main/views.py
+++ app/main/views.py
@@ -6,9 +6,26 @@ from .. import db,photos
 from flask_login import login_required, current_user
 import markdown2
 from .forms import *
+from ..request import getquote
 
 
+@main.route('/')
+def index():
+    '''
+    Index page
+    return
+    '''
+    
+    message= "Welc"
+    title= 'BLOG-app!'
+    
+    quote =getquote()
+    
+        
 
+    return render_template('index.html', message=message,title=title,quote = quote ) 
+
+   
 @main.route('/user/<uname>')
 @login_required
 def profile(uname):
@@ -18,6 +35,9 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+
+  
 
 @main.route('/user/<uname>/update',methods = ['GET','POST'])
 @login_required
@@ -41,7 +61,9 @@ def update_profile(uname):
 @main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
 def update_pic(uname):
+
     user = User.query.filter_by(username = uname).first()
+    
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
         path = f'photos/{filename}'
@@ -49,58 +71,104 @@ def update_pic(uname):
         db.session.commit()
     return redirect(url_for('main.profile',uname=uname))
 
-def bookings():
-
-    if not current_user.is_authenticated:
-        return redirect(url_for('core.home'))
-    context = {"user_has_booking":user_has_booking(),"user_bookings":get_user_bookings()}
-    return render_template('booking/dashboard.html', title='Booking Dashboard',data=context)
-
-
 
 @main.route('/booking/new_booking', methods = ['GET','POST'])
+@login_required
 def new_booking():
     
-    form = BookForm()
+    form = BookingForm()
 
     if form.validate_on_submit():
-        start_date_time = form. start_date_time.data
+        car_type = form.car_type.data
+        start_date_time = form.start_date_time.data
         end_date_time = form.end_date_time.data
         mobile = form.mobile.data
-        ID_numper = forms.ID_numper.data
+        ID_numper = form.ID_numper.data
         payment = form.payment.data
 
         
-        new_booking = Book(start_date_time= start_date_time,end_date_time= end_date_time,mobile = mobile,ID_numper = ID_numper, payment = payment,user_id=current_user.id)
+        new_booking = Booking(car_type=car_type,start_date_time= start_date_time,end_date_time= end_date_time,mobile = mobile,ID_numper = ID_numper, payment = payment,user_id=current_user.id)
 
         title='New booking'
 
         new_booking.save_booking()
+        
 
-        # return redirect(url_for('main.thankyou'))
+        return redirect(url_for('main.new_booking'))
 
-    return render_template('booking.html',form= form, )
+    return render_template('booking.html',form= form)
 
 
-# @booking.route('/bookings/new', methods=('GET', 'POST'))
-# def new_booking():
-#     if not current_user.is_authenticated:
-#         return redirect(url_for('core.home'))
 
-#     form = BookingForm()
-#     if form.validate_on_submit():
-#         print (form.car_id.data)
-#         booking = Booking(user_id=current_user.id,
-#                     car_id=form.car_id.data,
-#                     start_time=form.start_date_time.data,
-#                     end_time=form.end_date_time.data)
-#         booking_validation = booking.is_car_avaiable()
-#         if booking_validation['result'] == False:
-#             flash(booking_validation['reason'])
-#             return render_template('booking/new.html', title='New Booking', form=form)
-#         db.session.add(booking)
-#         db.session.commit()
-#         flash('Thanks for making a booking')
-#         return redirect(url_for('booking_blueprint.bookings'))
-#     return render_template('booking/new.html', title='New Booking',form=form)
+@main.route('/booking/all', methods=['GET', 'POST'])
+@login_required
+def all():
+    opinion = Booking.query.all()
+    return render_template( 'opinion.html',opinion=opinion)
+
+
+@main.route('/comments/<id>')
+@login_required
+def comment(id):
+    '''
+    function to return the comments
+    '''
+    comm =Comments.get_comment(id)
+    title = 'comments'
+    return render_template('comment.html',comment = comm,title = title)
+
+
+
+@main.route('/new_comment/', methods = ['GET','POST'])
+@login_required
+def new_comment():
+    # bookings = Book.query.filter_by(id = booking_id).first()
+    form = CommentForm()
+    comments = Comments.query.all()
+    if form.validate_on_submit():
+        username = form.username.data
+        # new_username = Comments(username=username,user_id=current_user.id)
+        # new_username.save_username() 
+
+        comment = form.comment.data
+        new_comment = Comments(comment=comment, username = username ,user_id=current_user.id)
+        new_comment.save_comment()
+        return redirect(url_for('main.new_comment'))
+    title='New comment'
+    return render_template('new_comment.html',title=title,comment_form = form ,comments=comments)
+
+
+def is_car_avaiable(self):
+        within_another_bookings_timeslot = db.session.query(Booking).filter(Booking.car_id == self.car_id,
+            Booking.start_time <= self.start_time,Booking.end_time >= self.end_time).count()
+
+        user_same_time_slot = db.session.query(Booking).filter(Booking.user_id == self.user_id,
+                                                                            Booking.start_time <= self.start_time,
+                                                                            Booking.end_time >= self.end_time).count()
+
+
+        booking_within_this_range = db.session.query(Booking).filter(Booking.car_id == self.car_id,
+            Booking.start_time >= self.start_time,Booking.end_time <= self.end_time).count()
+
+
+        starts_two_hours_before_end_start = db.session.query(Booking).filter(Booking.car_id == self.car_id,
+             Booking.end_time >= self.start_time - timedelta(hours=2),Booking.end_time <= self.start_time).count()
+
+        user_booking_count = db.session.query(Booking).filter(Booking.user_id == self.user_id).count()
+
+        if user_booking_count > 5:
+            return {"result": False,"reason":"You can not have more than 5 bookings"}
+        if user_same_time_slot > 0:
+            return {"result": False,"reason":" You already have a car already booked for these times."}
+        if booking_within_this_range > 0:
+            return {"result": False, "reason": " This car has already been booked between these times"}
+        if within_another_bookings_timeslot > 0:
+            return {"result": False, "reason": "This car has already been booked between these times"}
+        if starts_two_hours_before_end_start > 0:
+            return {"result": False, "reason": "Atleast 2 hours needed between bookings"}
+        return {"result": True, "reason": ""}     
+
+
+
+
 
